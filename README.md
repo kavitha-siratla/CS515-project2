af4ac514645b76ec4e36f136e9e45d38028761a5
kavitha siratla <kavithasiratla@kavithas-MacBook-Pro.local>
2023-04-17 23:50:29 -0400

    project

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # CS515-project2
+Kavitha Siratla [ksiratla@stevens.edu](mailto:ksiratla@stevens.edu)
+Rushiraj Herma [rherma@stevens.edu](mailto:rherma@stevens.edu)
+
+# the URL of your public GitHub repo
+https://github.com/Rushiraj0608
+
+# an estimate of how many hours you spent on the project
+45 hours
+
+# a description of how you tested your code
+
+When implementing a basic calculator project, it's important to test the code thoroughly to ensure that it's functioning correctly. In this particular project, the first step was to implement a lexical analyzer (lex) to generate a token stream. The token stream was then fed to the parser to generate an abstract syntax tree (AST), which was used to create an interpreter that generated the output.
+
+To test the code, we began by adding doctests to the individual functions to ensure that they were behaving as expected. This allowed us to test the individual components of the code before testing the entire project as a whole.
+
+Once we were confident that each individual component was functioning correctly, we tested the project step by step. We started by testing the lexer. Instead of directly supplying input from a text file, we initially tested it with a variable and passed it to the lexer function, checking the output by printing it out to the console. This approach allowed us to identify any issues with the lexer's implementation and ensure that it was generating the desired output.
+
+After verifying that the lexer was functioning correctly, we moved on to testing the parser and interpreter. We used a similar approach, passing input as a variable and checking the output by printing it out to the console. This helped us to ensure that the parser was generating the expected AST, and that the interpreter was correctly interpreting the AST to generate the desired output.
+
+Finally, we tested the entire project by using a text file as input. This allowed us to ensure that the entire system was working as intended, and that there were no issues or bugs that had been missed during testing.
+
+
+# bugs and issues
+
+# resolved issue
+
+
+# Implemented extensions
+# 1) Op-equals
+The Op-equals extension is a feature that allows compound assignment operators to be used in a programming language. These operators combine a binary operator with an assignment operation, such as adding or subtracting a value to a variable. The Op-equals extension adds support for six compound assignment operators, which include +=, -=, *=, /=, %=, and ^=.
+
+The meaning of these operators is straightforward: x op= e is equivalent to x = x op (e), where op is a binary operator, x is a variable, and e is an expression. For example, x += 1 is equivalent to x = x + 1. This means that the value of x is incremented by 1 and then assigned back to x. Similarly, x -= 1 would subtract 1 from the value of x and assign the result back to x.
+
+The Op-equals extension simplifies code by allowing more concise expressions to be written. For example, instead of writing x = x + 1, one can write x += 1. This makes the code shorter and easier to read, especially in cases where the expression is long or complex. Additionally, using compound assignment operators can make the code more efficient since it eliminates the need to recompute the value of x on the right-hand side of the assignment.
+
+In conclusion, the Op-equals extension is a useful feature that simplifies code and improves readability. It allows for the use of compound assignment operators, such as +=, -=, *=, /=, %=, and ^=, which can make expressions shorter and easier to read. By eliminating the need to recompute the value of a variable, this extension can also improve code efficiency.
+
+# 2) Relational operations
+The second extension, Relational operations, provides support for operators that allow comparisons between values. Relational operators include "==", "<=", ">=", "!=", "<", and ">". These operators are binary operators that return a boolean value of either 1 (true) or 0 (false) based on whether the comparison is true or false.
+
+For example, if we have two variables a and b, and we want to compare if a is greater than b, we would write "a > b". If a is indeed greater than b, the expression would evaluate to 1, otherwise, it would evaluate to 0. Similarly, we could check if two variables are equal by using the "==" operator.
+
+It's worth noting that these operators have lower precedence than arithmetic operators, meaning that arithmetic operations are performed before relational operations. For example, in the expression "3 + 4 < 7", the addition operation is performed first, and then the comparison operator "<" is applied to the result.
+
+By providing support for relational operators, this extension allows programmers to create more complex conditional statements and logic in their code. For instance, they can use relational operators to compare user input with predefined values or to test the validity of a given condition.
+
+
+# 3) Boolean operations
+The Boolean operations extension allows the calculator to perform logical operations on expressions. The && (conjunction) operator returns 1 if both operands are non-zero and 0 otherwise. The || (disjunction) operator returns 1 if either operand is non-zero and 0 otherwise. The ! (negation) operator reverses the truth value of its operand.
+
+In the example given, the code snippet "print 1 && 2, 2 && 1, -5 && 1, 0 && -100" uses the && operator to test various combinations of operands. The first two expressions both evaluate to 1 because both operands are non-zero. The third expression also evaluates to 1 because -5 is non-zero. The last expression evaluates to 0 because both operands are zero.
+
+It's important to note that Boolean operators have lower precedence than arithmetic and relational operators. For example, the expression "5 + 4 > 6 && 2 * 3 == 6" will be evaluated as "(5 + 4) > 6 && (2 * 3) == 6" due to operator precedence rules. Additionally, the && and || operators are left associative, meaning that expressions are evaluated from left to right. Finally, the ! operator is non-associative, meaning that it can only operate on a single operand.
+
+The Boolean operations extension allows for the creation of complex conditions and expressions in the calculator, enabling more advanced calculations and decision making.
+
+# 4) Comments
+
+The fourth extension, Multi-line comments and single-line comments, allows developers to add comments to their code. Multi-line comments begin with /* and end with */, and can span multiple lines of code. Arbitrary line breaks are allowed between the start and end of multi-line comments. For example,
+
+/* This is a
+multi-line comment
+*/
+Single-line comments begin with # and continue until the end of the line. They can be used anywhere in the code. For example,
+
+x = 1  # this sets x to 1
+Comments are not executed as part of the code and are purely for the benefit of the developer. They can be used to provide additional information about the code, such as why a particular approach was taken or to describe the purpose of a variable. This can improve code readability and help other developers understand the code. Additionally, comments can be used to temporarily remove or disable parts of the code for testing purposes, without actually deleting the code.
--- bc.py
+++ bc.py
@@ -1,18 +1,589 @@
+# LEXING
+
+from typing import Any
+
+
 with open("input.txt") as file:
     lines = [line.rstrip() for line in file]
 
-    exp = {}
-def assignmentFunction(line):
-    line = line.replace(" ","")
-    expression = line.split("=")
-    print(line)
-    # print(expression[0], " = " , expression[1])
-    exp[expression[0]] = expression[1]
-    print()
-for line in lines:
-    # print(line)
-    if("=" in line):
-        assignmentFunction(line)
+for i in range(len(lines)):
+    if '#' in lines[i]:
+        lines[i] = lines[i].split('#', 1)[0]
+    if '/*' in lines[i]:
+        lines[i] = lines[i].split('/*', 1)[0]
+        for j in range(i,len(lines)):
+            if '*/' in lines[i]:
+                lines[i] = lines[i].split('/*', 1)[1]
+            else:
+                lines.remove(lines[i])
 
-print(exp)
-    # print()
+input_lines = []
+from collections import defaultdict
+variables = defaultdict(float)
+    
+while True:
+    try: 
+        input_lines.append(input())
+    except KeyboardInterrupt:
+        break
+    except EOFError:
+        break
+
+
+class token():
+    typ: str
+    val: str
+
+    def __init__(self, typ, val):
+        """
+        >>> token('sym', '(')
+        token('sym', '(')
+        """
+        self.typ = typ
+        self.val = val
+
+    def __repr__(self):
+        return f'token({self.typ!r}, {self.val!r})'
+
+def lex(s: str) -> list[token]:
+    """
+    >>> lex('')
+    []
+    >>> lex('x=3')
+    [token('var', 'x'), token('asg', '='), token('int', '3')]
+    """
+
+    tokens = []
+    i = 0
+
+    while i < len(s):
+        if s[i].isspace():
+            i += 1
+        if s[i] == ',':
+            i += 1
+        elif s[i].isalpha():
+            end = i + 1
+            while end < len(s) and (s[end].isalnum() or s[end] == '_'):
+                end += 1
+            assert end >= len(s) or not (s[end].isalnum() or s[end] == '_')
+
+            word = s[i:end]
+
+            if word == 'print':
+                tokens.append(token('kw', word))
+            elif word == 'pi':
+                tokens.append(token('int', 3.14))
+            else:
+                tokens.append(token('var', word))
+            i = end
+        elif s[i].isdigit():
+            end = i + 1
+            while end < len(s) and (s[end].isdigit()):
+                end += 1
+            assert end >= len(s) or not (s[end].isdigit())
+
+            word = s[i:end]
+
+            if word.isdigit():
+                tokens.append(token('int', word))
+            i = end
+
+        elif s[i] == '_':
+            end = i + 1
+            while end < len(s) and (s[end].isalnum() or s[end] == '_'):
+                end += 1
+            assert end >= len(s) or not (s[end].isalnum() or s[end] == '_')
+
+            word = s[i:end]
+
+            tokens.append(token('var', word))
+            i = end
+
+        elif s[i] == '(':
+            tokens.append(token('sym', '('))
+            i += 1
+        elif s[i] == ')':
+            tokens.append(token('sym', ')'))
+            i += 1
+        elif s[i:i+2] == '++':
+            tokens.append(token('un', '++'))
+            i += 2
+        elif s[i:i+2] == '--':
+            tokens.append(token('un', '--'))
+            i += 2
+        elif s[i:i+1] == '+':
+            tokens.append(token('opr', '+'))
+            i += 1
+        elif s[i:i+1] == '-':
+            tokens.append(token('opr', '-'))
+            i += 1
+        elif s[i:i+1] == '*':
+            tokens.append(token('opr', '*'))
+            i += 1
+        elif s[i:i+1] == '/':
+            tokens.append(token('opr', '/'))
+            i += 1
+        elif s[i:i+1] == '%':
+            tokens.append(token('opr', '%'))
+            i += 1
+        elif s[i:i+1] == '^':
+            tokens.append(token('opr', '^'))
+            i += 1
+        elif s[i:i+1] == '=':
+            tokens.append(token('asg', '='))
+            i += 1
+        elif s[i:i+2] == '+=':
+            tokens.append(token('opeq', '+='))
+            i += 2
+        elif s[i:i+2] == '-=':
+            tokens.append(token('opeq', '-='))
+            i += 2
+        elif s[i:i+2] == '*=':
+            tokens.append(token('opeq', '*='))
+            i += 2
+        elif s[i:i+2] == '/=':
+            tokens.append(token('opeq', '/='))
+            i += 2
+        elif s[i:i+2] == '%=':
+            tokens.append(token('opeq', '%='))
+            i += 2
+        elif s[i:i+2] == '^=':
+            tokens.append(token('opeq', '^='))
+            i += 2
+        elif s[i:i+2] == '||':
+            tokens.append(token('sym', '||'))
+            i += 2
+        elif s[i:i+2] == '&&':
+            tokens.append(token('sym', '&&'))
+            i += 2
+        elif s[i] == '!':
+            tokens.append(token('sym', '!'))
+            i += 1
+        elif s[i:i+2] == '==':
+            tokens.append(token('relop', '=='))
+            i += 2
+        elif s[i:i+2] == '<=':
+            tokens.append(token('relop', '<='))
+            i += 2
+        elif s[i:i+2] == '>=':
+            tokens.append(token('relop', '>='))
+            i += 2
+        elif s[i:i+2] == '!=':
+            tokens.append(token('relop', '!='))
+            i += 2
+        elif s[i:i+1] == '>':
+            tokens.append(token('relop', '>'))
+            i += 1
+        elif s[i:i+1] == '<':
+            tokens.append(token('relop', '<'))
+            i += 1
+        else:
+            raise SyntaxError(f'unexpected character {s[i]}')
+
+    return tokens
+
+
+# PARSING
+
+
+class ast():
+    typ: str
+    children: tuple[Any, ...]
+
+    def __init__(self, typ: str, *children: Any):
+        """
+        x || true
+        >>> ast('||', ast('var', 'x'), ast('val', True))
+        ast('||', ast('var', 'x'), ast('val', True))
+        """
+        self.typ = typ
+        self.children = children
+
+    def __repr__(self):
+        return f'ast({self.typ!r}, {", ".join([repr(c) for c in self.children])})'
+
+def parse(s: str) -> ast:
+    ts = lex(s)
+
+    a, i = disj(ts, 0)
+
+    if i != len(ts):
+        raise SyntaxError(f"expected EOF, found {ts[i:]!r}")
+
+    return a
+
+
+def disj(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('true || false')
+    ast('||', ast('val', True), ast('val', False))
+    """
+    if i >= len(ts):
+        raise SyntaxError('expected conjunction, found EOF')
+
+    lhs, i = conj(ts, i)
+
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '||':
+        rhs, i = conj(ts, i+1)
+        lhs = ast('||', lhs, rhs)
+
+    return lhs, i
+
+def conj(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('true && false')
+    ast('&&', ast('val', True), ast('val', False))
+    >>> parse('!x && (a && !false)')
+    ast('&&', ast('!', ast('var', 'x')), ast('&&', ast('var', 'a'), ast('!', ast('val', False))))
+    >>> parse('!x && a && !false')
+    ast('&&', ast('&&', ast('!', ast('var', 'x')), ast('var', 'a')), ast('!', ast('val', False)))
+    """
+    if i >= len(ts):
+        raise SyntaxError('expected conjunction, found EOF')
+
+    lhs, i = assign(ts, i)
+
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '&&':
+        rhs, i = assign(ts, i+1)
+        lhs = ast('&&', lhs, rhs)
+
+    return lhs, i
+
+
+# def printer(ts: list[token], i: int) -> tuple[ast, int]:
+#     """
+#     >>> parse('true && false')
+#     ast('&&', ast('val', True), ast('val', False))
+#     >>> parse('!x && (a && !false)')
+#     ast('&&', ast('!', ast('var', 'x')), ast('&&', ast('var', 'a'), ast('!', ast('val', False))))
+#     >>> parse('!x && a && !false')
+#     ast('&&', ast('&&', ast('!', ast('var', 'x')), ast('var', 'a')), ast('!', ast('val', False)))
+#     """
+#     if i >= len(ts):
+#         raise SyntaxError('expected conjunction, found EOF')
+
+#     lhs, i = assign(ts, i)
+
+#     while i < len(ts) and ts[i-1].typ == 'kw' and ts[i-1].val == 'print':
+#         rhs, i = assign(ts, i)
+#         lhs = ast('print', rhs)
+
+#     return lhs, i
+
+def assign(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected assignment, found EOF')
+
+    lhs, i = uninc(ts,i)
+    if i<len(ts) and ts[i].typ == 'asg' and ts[i].val == '=':
+        rhs, i = uninc(ts, i+1)
+        lhs =  ast('=', lhs, rhs)
+
+    return lhs, i
+
+def uninc(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected unary increment, found EOF')
+
+    lhs, i = undec(ts,i)
+
+    while i < len(ts) and ts[i].typ == 'un' and ts[i].val == '++':
+        lhs = ast('++', lhs)
+        i += 1
+
+    return lhs, i
+
+def undec(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected unary decrement, found EOF')
+
+    lhs, i = expo(ts,i)
+    while i < len(ts) and ts[i].typ == 'un' and ts[i].val == '--':
+        lhs = ast('--', lhs)
+        i += 1
+    return lhs, i
+
+def expo(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected divisiom, found EOF')
+
+    lhs, i = mod(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '^':
+        rhs, i = mod(ts, i+1)
+        lhs =  ast('^', lhs, rhs)
+
+    return lhs, i
+
+def mod(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected divisiom, found EOF')
+
+    lhs, i = div(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '%':
+        rhs, i = div(ts, i+1)
+        lhs =  ast('%', lhs, rhs)
+
+    return lhs, i
+
+def div(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected divisiom, found EOF')
+
+    lhs, i = mul(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '/':
+        rhs, i = mul(ts, i+1)
+        lhs =  ast('/', lhs, rhs)
+
+    return lhs, i
+
+def mul(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected multiplication, found EOF')
+
+    lhs, i = add(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '+':
+        rhs, i = add(ts, i+1)
+        lhs =  ast('*', lhs, rhs)
+
+    return lhs, i
+
+def add(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected addition, found EOF')
+
+    lhs, i = sub(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '+':
+        rhs, i = sub(ts, i+1)
+        lhs =  ast('+', lhs, rhs)
+
+    return lhs, i
+
+def sub(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected subtraction, found EOF')
+
+    lhs, i = groreq(ts,i)
+    if i<len(ts) and ts[i].typ == 'opr' and ts[i].val == '-':
+        rhs, i = groreq(ts, i+1)
+        lhs =  ast('-', lhs, rhs)
+
+    return lhs, i
+
+def groreq(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected greter or equal, found EOF')
+
+    lhs, i = leoreq(ts,i)
+    if i<len(ts) and ts[i].typ == 'relop' and ts[i].val == '>=':
+        rhs, i = leoreq(ts, i+1)
+        lhs =  ast('>=', lhs, rhs)
+
+    return lhs, i
+
+def leoreq(ts: list[token], i: int) -> tuple[ast, int]:
+    
+    if i >= len(ts):
+        raise SyntaxError('expected greter or equal, found EOF')
+
+    lhs, i = neg(ts,i)
+    if i<len(ts) and ts[i].typ == 'relop' and ts[i].val == '<=':
+        rhs, i = neg(ts, i+1)
+        lhs =  ast('<=', lhs, rhs)
+
+    return lhs, i
+
+
+
+
+def neg(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('! true')
+    ast('!', ast('val', True))
+    >>> parse('!! true')
+    ast('!', ast('!', ast('val', True)))
+    """
+
+    if i >= len(ts):
+        raise SyntaxError('expected negation, found EOF')
+
+    if ts[i].typ == 'sym' and ts[i].val == '!':
+        a, i = neg(ts, i+1)
+        return ast('!', a), i
+    else:
+        return atom(ts, i)
+
+
+
+
+def atom(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('x')
+    ast('var', 'x')
+    >>> parse('true')
+    ast('val', True)
+    >>> parse('(((false)))')
+    ast('val', False)
+    """
+
+    t = ts[i]
+
+    if t.typ == 'var':
+        return ast('var', t.val), i+1
+    if t.typ == 'int':
+        return ast('int', t.val), i+1
+    if t.typ == 'opr':
+        return ast('opr', t.val), i+1
+    # elif t.typ == 'kw' and t.val in ['print']:
+    #     return ast('kw', t.val), i + 1
+    elif t.typ == 'kw' and t.val in ['true', 'false']:
+        return ast('val', t.val == 'true'), i + 1
+    elif t.typ == 'sym' and t.val == '(':
+        a, i = disj(ts, i + 1)
+
+        if i >= len(ts):
+            raise SyntaxError(f'expected right paren, got EOF')
+
+        if not (ts[i].typ == 'sym' and ts[i].val == ')'):
+            raise SyntaxError(f'expected right paren, got "{ts[i]}"')
+        
+        return a, i + 1
+
+    raise SyntaxError(f'expected atom, got "{ts[i]}"')
+
+# INTERPRETER
+
+def checkIfVar(a: ast, env: set[str]):
+    try:
+        if a.typ == 'var':
+            return (True, a.children[0])
+    except:
+        print("Error occured!")
+
+def interp(a: ast, env: set[str]):
+    global variables
+    try:
+        if a.typ == 'val':
+            return a.children[0]
+        elif a.typ == 'int':
+            return float(a.children[0])
+        # elif a.typ == 'print':
+        #     print(interp(a.children[0],env))
+        #     return
+        elif a.typ == 'var':
+            return variables[a.children[0]]
+        elif a.typ == '!':
+            return not interp(a.children[0], env)
+        elif a.typ == '=':
+            var = checkIfVar(a.children[0], env)
+            if var[0]:
+                variables[var[1]] = interp(a.children[1], env)   
+        elif a.typ == '+':
+            return interp(a.children[0], env) + interp(a.children[1], env)
+        elif a.typ == '-':
+            return interp(a.children[0], env) - interp(a.children[1], env)
+        elif a.typ == '*':
+            return interp(a.children[0], env) * interp(a.children[1], env)
+        elif a.typ == '/':
+            return interp(a.children[0], env) / interp(a.children[1], env)
+        elif a.typ == '%':
+            return interp(a.children[0], env) % interp(a.children[1], env)
+        elif a.typ == '^':
+            return interp(a.children[0], env) ** interp(a.children[1], env)
+        elif a.typ == '--':
+            return interp(a.children[0], env)-1
+        elif a.typ == '++':
+            return interp(a.children[0], env)+1
+        elif a.typ == '+=':
+            return interp(a.children[0], env) + interp(a.children[1], env)
+        elif a.typ == '-=':
+            return interp(a.children[0], env) - interp(a.children[1], env)
+        elif a.typ == '*=':
+            return interp(a.children[0], env) * interp(a.children[1], env)
+        elif a.typ == '/=':
+            return interp(a.children[0], env) / interp(a.children[1], env)
+        elif a.typ == '%=':
+            return interp(a.children[0], env) % interp(a.children[1], env)
+        elif a.typ == '^=':
+            return interp(a.children[0], env) ^ interp(a.children[1], env)
+        elif a.typ == '==':
+            return interp(a.children[0], env) == interp(a.children[1], env)
+        elif a.typ == '<=':
+            return interp(a.children[0], env) <= interp(a.children[1], env)
+        elif a.typ == '>=':
+            return interp(a.children[0], env) >= interp(a.children[1], env)
+        elif a.typ == '!=':
+            return interp(a.children[0], env) != interp(a.children[1], env)
+        elif a.typ == '<':
+            return interp(a.children[0], env) < interp(a.children[1], env)
+        elif a.typ == '>':
+            return interp(a.children[0], env) > interp(a.children[1], env)
+        elif a.typ == '!':
+            return not interp(a.children[0], env)
+        elif a.typ == '&&':
+            return interp(a.children[0], env) and interp(a.children[1], env)
+        elif a.typ == '||':
+            return interp(a.children[0], env) or interp(a.children[1], env)
+        
+    except ZeroDivisionError:
+        return 'divide by zero'
+    
+
+# with open("input.txt") as file:
+#     lines = [line.rstrip() for line in file]
+
+# for i in lines:
+#     ast = parse(i)
+
+
+# expr = 'true || false && !x'
+for line in input_lines:
+    expr = line
+    if line.strip().startswith('print'):
+        line = line.replace('print',"")
+        pvars= line.split(',')
+        lineResult = ""
+        for pvar in pvars:
+            lineResult = lineResult + str ( interp(parse(pvar),{'x'}) ) + " "
+        print(lineResult.strip())
+    else:
+        result = parse(expr)
+        interp(result,{'x'})
+        print(str(variables))
+        print(result)
+# print(ast.children[0])
+# print(ast.children[0].children[0])
+# print(ast.children[1]) 
+# print(ast.children[1].children[0])  
+
+# expr = 'true || false && !x'
+# ast = parse(expr)
+# print(ast.typ)
+# print(ast.children[0].typ)
+# print(ast.children[0].children[0])
+# print(ast.children[1].typ)
+# print(ast.children[1].children[0].typ) 
+
+
+# if l = "print ascnao, wefwe, 46884*324523, 32"
+
+# l.trim()
+ 
+# results = [1, ]
+# // print 1, x, 3, 4-5
+# if (l[0:5] === print):
+        # values = [1, x, 3, 4-5]
+        # ast(1,;int ,, x, var)
+#     values = l.split(',')
+#     value -> input
+#     results.append
+    
